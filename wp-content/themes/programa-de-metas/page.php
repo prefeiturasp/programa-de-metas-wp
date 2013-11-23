b20f73dd8dae91a203845e5c7eced9d03a2dd37d
GoncaWindows <gabriel.goncalves@ppivot.com.br>
2013-11-23 03:35:55 -0200

    tirando italico e bold de internas

--- wp-content/themes/programa-de-metas/page.php
+++ wp-content/themes/programa-de-metas/page.php
@@ -13,7 +13,9 @@
 	
 	<div class="content interna">
 		<h1 class="logo logo-interna">
-			<img src="<?php echo get_template_directory_uri(); ?>/img/logo-programa-de-metas.png">
+			<a href="<?php echo bloginfo('url');?>/">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/logo-programa-de-metas.png">
+			</a>
 		</h1>
 		
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>

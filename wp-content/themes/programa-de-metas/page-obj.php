d3a5489f92f55953c6eb722f0e7b51bb5bcec9da
GoncaWindows <gabriel.goncalves@ppivot.com.br>
2013-11-23 01:55:09 -0200

    template de paginas estaticas

---
+++ wp-content/themes/programa-de-metas/page-obj.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Template Name: Objetivos e Metas
+*/
+?>
+<?php get_header(); ?>
+<div id="all" class="interna">
+	<div class="nav">
+		<div class="content">
+			<ul>
+				<li><a href="<?php echo bloginfo('url');?>/conheca-o-programa/" class="first">Conheça o programa</a>|</li>
+				<li><a href="<?php echo bloginfo('url');?>/objetivos-e-metas/">Objetivos e metas</a>|</li>
+				<li><a href="<?php echo bloginfo('url');?>/conceito-territorial/">Conceito territorial</a>|</li>
+				<li><a href="">Contato</a></li>
+			</ul>
+		</div>
+	</div>
+	
+	<div class="content interna">
+		<h1 class="logo logo-interna">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/logo-programa-de-metas.png">
+		</h1>
+		
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+			<?php the_content(); ?>
+		<?php endwhile; ?>
+		<?php endif; ?>
+	</div>
+</div>

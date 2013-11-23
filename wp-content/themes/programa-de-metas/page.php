705a248893dcb4c5106c3f4da8aa84fc17dbd908
GoncaWindows <gabriel.goncalves@ppivot.com.br>
2013-11-23 00:38:30 -0200

    estrutura de paginas estaticas

--- wp-content/themes/programa-de-metas/page.php
+++ wp-content/themes/programa-de-metas/page.php
@@ -1,43 +1,24 @@
 <?php get_header(); ?>
+<div id="all">
+	<div class="nav">
+		<div class="content">
+			<ul>
+				<li><a href="" class="first">Conheça o programa</a>|</li>
+				<li><a href="">Objetivos e metas</a>|</li>
+				<li><a href="">Conceito territorial</a>|</li>
+				<li><a href="">Contato</a></li>
+			</ul>
+		</div>
+	</div>
 	
-	<!-- section -->
-	<section role="main">
-	
-		<h1><?php the_title(); ?></h1>
-	
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-	
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="content interna">
+		<h1 class="logo logo-interna">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/logo-programa-de-metas.png">
+		</h1>
 		
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 			<?php the_content(); ?>
-			
-			<?php comments_template( '', true ); // Remove if you don't want comments ?>
-			
-			<br class="clear">
-			
-			<?php edit_post_link(); ?>
-			
-		</article>
-		<!-- /article -->
-		
-	<?php endwhile; ?>
-	
-	<?php else: ?>
-	
-		<!-- article -->
-		<article>
-			
-			<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-			
-		</article>
-		<!-- /article -->
-	
-	<?php endif; ?>
-	
-	</section>
-	<!-- /section -->
-	
-<?php get_sidebar(); ?>
-
-<?php get_footer(); ?>
+		<?php endwhile; ?>
+		<?php endif; ?>
+	</div>
+</div>

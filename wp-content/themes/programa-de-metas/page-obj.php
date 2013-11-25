f9f6ca0ad159b38cc16817b5e453c9af491154bf
Gabriel Felipe Gonçalves <gabriel@hmsinternet.com>
2013-11-25 18:44:29 -0200

    botoes socias

--- wp-content/themes/programa-de-metas/page-obj.php
+++ wp-content/themes/programa-de-metas/page-obj.php
@@ -13,6 +13,21 @@ Template Name: Objetivos e Metas
 				<li><a href="<?php echo bloginfo('url');?>/conceito-territorial/">Conceito territorial</a>|</li>
 				<li><a href="http://planejasampa.prefeitura.sp.gov.br/index.php/contato/">Contato</a></li>
 			</ul>
+			
+			<div class="social">
+				<div class="fb">
+				    <div class="fb-share-button" data-href="http://programademetas.info" data-type="button" data-width="120"></div>
+				</div>
+				
+				<div class="tw">
+				    <a href="https://twitter.com/share?url=<?php echo bloginfo('url');?>&text=Programa%20de%20Metas%20da%20Cidade%202013%20-2016%3A%20um%20convite%20ao%20planejamento%20urbano%20participativo." class="twitter-share-button" data-lang="pt">Tweet</a>
+				    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+				</div>
+				
+				<div class="gp">
+				    <g:plus action="share" annotation="none"></g:plus>
+				</div>
+			</div>
 		</div>
 	</div>
 	

785273d7e10d9c97b8bf590de56fad0853694e11
Gabriel Felipe Gonçalves <gabriel@hmsinternet.com>
2013-11-25 18:35:11 -0200

    botoes socias

--- wp-content/themes/programa-de-metas/header.php
+++ wp-content/themes/programa-de-metas/header.php
@@ -21,9 +21,22 @@
 		<script type="text/javascript">
 			var wpAjaxUrl = '<?php echo admin_url('admin-ajax.php');?>';
 		</script>
+		
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1&appId=540906732661113";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+		
+		<meta property="og:type" content="site" />
+		<meta property="og:title" content="Programa de Metas da Cidade de São Paulo" />
+		<meta property="og:description" content="O esforço de elaboração do Programa de Metas 2013-2016 foi o de ir além da lista de metas, apontando objetivos estratégicos, eixos estruturantes e articulações territoriais sobre os quais se pretende alcançar resultados efetivos." />
+		<meta property="og:image" content=<?php echo get_template_directory_uri(); ?>/img/logo-facebook.jpg" />
 	</head>
 	<body <?php body_class(); ?>>
-	
+		<div id="fb-root"></div>
 		<!-- wrapper -->
 		<!--div class="wrapper"-->
 			<div class="mask"></div>

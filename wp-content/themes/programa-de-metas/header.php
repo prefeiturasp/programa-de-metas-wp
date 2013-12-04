4e1c026e099554f486661988799d22f19929fa95
GoncaWindows <gabriel.goncalves@ppivot.com.br>
2013-12-04 18:18:01 -0200

    tirando icone de touch

--- wp-content/themes/programa-de-metas/header.php
+++ wp-content/themes/programa-de-metas/header.php
@@ -14,7 +14,7 @@
 		
 		<!-- icons -->
 		<!--link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon"-->
-		<link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+		<!--link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed"-->
 			
 		<!-- css + javascript -->
 		<?php wp_head(); ?>

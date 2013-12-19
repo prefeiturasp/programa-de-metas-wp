fd466d7a836a817135bd690488264fd92f832523
GoncaWindows <gabriel.goncalves@ppivot.com.br>
2013-12-19 08:23:36 -0200

    novos modelos de pagina

---
+++ wp-content/themes/programa-de-metas/page-conheca.php
@@ -0,0 +1,101 @@
+<?php
+/*
+Template Name: Conheça
+*/
+?>
+<?php get_header(); ?>
+<div id="all" class="interna">
+	<div class="nav">
+		<div class="content">
+			<ul>
+				<li><a href="<?php echo bloginfo('url');?>/conheca-o-programa/" class="first">Conheça o programa</a>|</li>
+				<li><a href="<?php echo bloginfo('url');?>/objetivos-e-metas/">Objetivos e metas</a>|</li>
+				<li><a href="<?php echo bloginfo('url');?>/conceito-territorial/">Conceito territorial</a>|</li>
+				<li><a href="http://planejasampa.prefeitura.sp.gov.br/index.php/contato/">Contato</a></li>
+			</ul>
+			
+			<div class="social">
+				<div class="fb">
+				    <div class="fb-share-button" data-href="http://programademetas.info" data-type="button" data-width="120"></div>
+				</div>
+				
+				<div class="tw">
+				    <a href="https://twitter.com/share?url=<?php echo bloginfo('url');?>&text=Programa%20de%20Metas%20da%20Cidade%202013%20-2016%3A%20um%20convite%20ao%20planejamento%20urbano%20participativo." class="twitter-share-button" data-lang="pt">Tweet</a>
+				    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+				</div>
+				
+				<div class="gp">
+				    <g:plus action="share" annotation="none"></g:plus>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="content interna conheca">
+		<h1 class="logo logo-interna">
+			<a href="<?php echo bloginfo('url');?>/">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/logo-programa-de-metas.png">
+			</a>
+		</h1>
+        
+        <h2>
+            O esforço de elaboração do Programa de Metas 2013-2016 foi o de ir além da lista de metas, apontando objetivos estratégicos, eixos estruturantes e articulações territoriais sobre os quais se pretende alcançar resultados efetivos. 
+        </h2>
+        
+        <p class="azul">
+            Tais objetivos, eixos e articulações, em conjunto com a participação popular no processo, são a verdadeira ponte<br />para a elaboração de um projeto de cidade. A possibilidade de transformação desse projeto em realidade passa<br />pela execução das metas, mas passa também pela continuidade do acompanhamento desses aspectos<br />estratégicos, pela capacidade de ajuste de percurso e, principalmente, pela apropriação desse projeto pela<br />população. O Programa de Metas 2013-2016 pode ser o começo de uma grande mudança. Esperamos que os<br />paulistanos aceitem o convite e nos acompanhem na construção desse novo modelo de planejamento público e<br />compreensão da ação sobre a cidade.
+        </p>
+        
+        <p class="left">
+            Em março de 2013, a Prefeitura de São Paulo apresentou à população da cidade o Programa de<br />
+            Metas da Prefeitura de São Paulo. Após a apresentação dessa proposta, foram realizadas no mês<br />
+            de abril 35 audiências públicas, sendo uma em cada uma das 31 subprefeituras, 3 audiências<br />
+            públicas temáticas – de acordo com os 3 eixos temáticos do Programa de Metas – e uma geral, na<br />
+            Câmara dos Vereadores de São Paulo. Em conformidade com o compromisso da atual gestão com<br />
+            a participação popular, foram recebidas propostas por meio de falas e por escrito nas audiências,<br />
+            além de propostas por e-mail. Todas as propostas foram sistematizadas, classificadas e avaliadas<br />
+            sobre a possibilidade de sua incorporação. O resultado deste processo está consolidado nesta<br />
+            Versão Final Participativa do Programa de Metas da Cidade de São Paulo 2013-2016 que agora vem a público.
+        </p>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/conheca/123.png" class="metas" />
+        
+        <p class="normal">
+            O Programa de Metas é uma exigência da Lei Orgânica do Município de São Paulo desde 2008, ano no qual uma mobilização da sociedade civil conseguiu<br />
+            fazer com que a Câmara Municipal aprovasse a criação do Programa de Metas. A partir daquele momento, todo prefeito eleito tem a obrigação de<br />
+            apresentar, em até noventa dias após a sua posse, um Programa que descreva as prioridades de seu governo, explicitando as ações estratégicas, os<br />
+            indicadores e as metas quantitativas para cada um dos setores da administração pública municipal. O Programa de Metas é, portanto, uma maneira de o<br />
+            governo selar compromissos com a população em torno das principais iniciativas que serão implementadas ao longo da gestão.
+        </p>
+        
+        <p class="normal">
+            Nesse sentido, o Programa de Metas da Cidade de São Paulo 2013-2016 pode ser entendido como a consolidação do programa de governo: Um Tempo<br />
+            Novo Para São Paulo, que, em 2012, foi escolhido nas urnas pela maioria da população paulistana e foi submetido à consulta dessa população através de<br />
+            audiências públicas. Para construí-lo, a Secretaria Municipal do Planejamento, Orçamento e Gestão comandou um processo de consulta a mais de duas<br />
+            dezenas de órgãos da administração direta e indireta. De posse desse conjunto inicial de dados, prospecções e aspirações, o desafio foi construir, no<br />
+            exíguo prazo disponível, um planejamento não só factível, como coerente e afinado com a visão de cidade defendida no programa de governo que saiu vitorioso das eleições.
+        </p>
+        
+        <img src="<?php echo get_template_directory_uri(); ?>/img/conheca/roda.png" align="right" />
+        
+        <p class="normal">
+            Se o Programa tem de expressar a visão de cidade que São Paulo e sua população aspiram para si, então ele deve ser muito mais do que pura e<br />
+            simplesmente um “listão” de metas quantitativas. O Programa deve traduzir essa visão num sistema de planejamento, que ordene os compromissos do<br />
+            executivo para colocar suas ações no rumo desejado. Metodologicamente, o caminho encontrado foi partir de objetivos estratégicos, aos quais as metas<br />
+            encontram-se associadas, e organizar as referidas metas segundo eixos temáticos e articulações territoriais. Por razões que já adiantamos, mas que<br />
+            ficarão mais claras mais à frente, estas duas diferentes clivagens que organizam o conjunto das metas complementam-se e são absolutamente<br />
+            indispensáveis uma à outra. Por outro lado, os objetivos estratégicos traduzem-se em indicadores que permitem acompanhar os efeitos esperados do<br />
+            Programa de Metas na situação concreta de vida da população.
+        </p>
+        
+        <p class="normal">
+            Vale enfatizar que a consecução de um Programa de Metas que prima pela organicidade só foi possível pela existência prévia de um programa de<br />
+            governo, que deu o norte para que esta peça de planejamento pudesse ser produzida e discutida com a população. No que se segue, tentaremos<br />
+            apresentar de modo sumário, mas, espera-se, compreensível, tal estrutura orgânica, mas, antes disso, faremos um balanço das audiências públicas e de<br />
+            sua contribuição para ela.
+        </p>
+        
+        <img src="<?php echo get_template_directory_uri(); ?>/img/conheca/cafe.png" align="right" class="cafe"/>
+	</div>
+</div>
+
+<?php get_footer();?>
---
+++ wp-content/themes/programa-de-metas/page-territorial.php
@@ -0,0 +1,94 @@
+<?php
+/*
+Template Name: Conceito Territorial
+*/
+?>
+<?php get_header(); ?>
+<div id="all" class="interna">
+	<div class="nav">
+		<div class="content">
+			<ul>
+				<li><a href="<?php echo bloginfo('url');?>/conheca-o-programa/" class="first">Conheça o programa</a>|</li>
+				<li><a href="<?php echo bloginfo('url');?>/objetivos-e-metas/">Objetivos e metas</a>|</li>
+				<li><a href="<?php echo bloginfo('url');?>/conceito-territorial/">Conceito territorial</a>|</li>
+				<li><a href="http://planejasampa.prefeitura.sp.gov.br/index.php/contato/">Contato</a></li>
+			</ul>
+			
+			<div class="social">
+				<div class="fb">
+				    <div class="fb-share-button" data-href="http://programademetas.info" data-type="button" data-width="120"></div>
+				</div>
+				
+				<div class="tw">
+				    <a href="https://twitter.com/share?url=<?php echo bloginfo('url');?>&text=Programa%20de%20Metas%20da%20Cidade%202013%20-2016%3A%20um%20convite%20ao%20planejamento%20urbano%20participativo." class="twitter-share-button" data-lang="pt">Tweet</a>
+				    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+				</div>
+				
+				<div class="gp">
+				    <g:plus action="share" annotation="none"></g:plus>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="content interna territorial">
+		<h1 class="logo logo-interna">
+			<a href="<?php echo bloginfo('url');?>/">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/logo-programa-de-metas.png">
+			</a>
+		</h1>
+        
+        <h2>
+            As ações de governo – obras, programas ou serviços – modificam as condições de vida e incidem na alteração ou consolidação da lógica de estruturação urbana. O Programa de Metas 2013-2016 e seu conjunto de iniciativas busca uma orientação territorial muito clara: a redução expressiva das desigualdades socioespaciais para a estruturação de uma cidade reconhecida como lugar em que se vive com qualidade e que oferece oportunidades para todos.
+        </h2>
+        
+        <p class="normal">
+            A visão enunciada deverá ser reforçada por um novo pacto que garanta uma política urbana em direção à justiça territorial. A revisão<br />
+            participativa do marco regulatório – que inclui revisão do Plano Diretor Estratégico (PDE), da Lei de Parcelamento, Uso e Ocupação<br />
+            do Solo (LPUOS) e dos Planos Regionais Estratégicos (PREs) – busca criar condições e garantias para que os interesses da<br />
+            coletividade se afirmem e direcionem as políticas públicas municipais. Da mesma forma, regula a produção privada do<br />
+            espaço urbano e consolida os instrumentos para a gestão democrática da cidade.
+        </p>
+        
+        <p class="small">
+            Nesse contexto, destacam-se dois aspectos fundamentais da Emenda 30 à L ei Orgânica do Município: a obrigatoriedade das metas serem regionalizadas<br />
+            e a exigência de que estejam em conformidade com o Plano Diretor vigente. Considerando esses dois princípios e partindo das macrozonas e macroáreas<br />
+            estabelecidas pela Lei nº 13.430/2002, são propostas cinco articulações territoriais:
+        </p>
+        
+        <div class="bottom">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/conceito-territorial/globo.png">
+            <div class="numeros">
+                <p>
+                    <b>1.</b> Resgate da cidadania nos territórios mais vulneráveis;<br />
+                    <b>2</b>. Estruturação do Arco do Futuro;<br />
+                    <b>3.</b> Fortalecimento das centralidades locais e das redes de equipamentos públicos;<br />
+                    <b>4.</b> Requalificação da área central;<br />
+                    <b>5.</b> Reordenação das bordas da cidade.<br />
+                </p>
+            </div>
+            
+            <div class="quadro">
+                <p>
+                    As articulações envolvem conjuntos de metas e estabelecem a combinação entre duas
+                    dimensões distintas da sua regionalização: (i) a dimensão espacial das ações setoriais, que
+                    apontam para territórios adequados para cada política pública; e (ii) a dimensão espacial
+                    do novo modelo de desenvolvimento urbano, que aponta a política adequada para cada
+                    território. Tal combinação cria bases para que a visão de cidade permeie, além deste
+                    Programa de Metas, o conjunto dos instrumentos de compõem o sistema municipal de
+                    planejamento e que incidem sobre diferentes temporalidades: os Planos Plurianuais, as
+                    Leis de Diretrizes Orçamentárias e as Leis Orçamentárias Anuais. Ao mesmo tempo,
+                    permite monitorar e avaliar as ações de governo segundo a lógica territorial proposta.
+                    Consolida-se, com isso, um salto qualitativo em direção a uma mudança estrutural na
+                    cidade de São Paulo. No que se segue apresentaremos a lógica que presidiu a definição de
+                    cada uma dessas cinco articulações, bem como exemplos de metas que as tornarão
+                    realidade. Ao final deste volume, no anexo, o leitor interessado encontrará o conjunto das
+                    metas que estão associadas a cada uma delas. Como será fácil perceber, há metas que se
+                    encontram em mais de uma das articulações territoriais, o que é da natureza desse tipo de organização.
+                </p>
+            </div>
+        </div>
+	</div>
+</div>
+
+<?php get_footer();?>

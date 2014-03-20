072292b4589325f55152b51c2cfd2b4144d2cb91
Lucas Pirola <lucaspirola@gmail.com>
2014-03-20 10:52:08 -0300

    adicionado suporte ao grunt no diretorio do tema para facilitar o desenvolvimento com sass

---
+++ wp-content/themes/pdm-andamento/assets/scripts/main.js
@@ -0,0 +1,48 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/dist/jquery',
+        d3: '../bower_components/d3js/build/d3.v3',
+        leaflet: '../bower_components/leaflet/src/Leaflet',
+        foundation: '../bower_components/foundation/js/foundation'
+    },
+    shim: {
+        d3: {
+            exports: 'd3'
+        },
+        jQuery: {
+            exports: 'jquery'
+        }
+    }
+});
+
+require(['chart', 'map', 'app', 'jquery'], function (chart, map, app, $) {
+    'use strict';
+    // use app here
+    app.init();
+
+    // startup map
+    $('.map-render').each(function () {
+        map.init(this);
+    });
+
+    // startup chart
+    $('.chart-render').each(function () {
+        var data = $(this).data('chart');
+        switch (data.type) {
+           case 'pie':
+              switch (data.size) {
+                  case 'small':
+                      chart.pieSmall(this, data.values);
+                      break;
+                  case 'large':
+                      chart.pieLarge(this, data.values);
+                      break;
+              }
+              break;
+           case 'line':
+              chart.line(this, data.values[0], data.values[1]);
+              break;
+        }
+    });
+
+});

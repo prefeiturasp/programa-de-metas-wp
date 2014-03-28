1537e31b808bf5ff61e365275fa1b159a34abafb
Lucas Pirola <lucaspirola@gmail.com>
2014-03-28 08:44:54 -0300

    chamadas para status das metas

--- wp-content/themes/pdm-andamento/src/PaginaInicial.php
+++ wp-content/themes/pdm-andamento/src/PaginaInicial.php
@@ -7,20 +7,20 @@ class PaginaInicial
     public static function get_context()
     {
         $context = \Timber::get_context();
-        $api = new ApiClient;
-        $context['metas'] = $api->getMetasFiltradas($projeto_id);
-        $context['subprefeituras'] = $api->getSubPrefeituras();
-        $context['objetivos'] = $api->getObjetivos();
-        $context['secretarias'] = $api->getSecretarias();
-        $context['eixos'] = $api->getEixos();
-        $context['articulacoes'] = $api->getArticulacoes();
+        // $api = new ApiClient;
+        // $context['metas'] = $api->getMetasFiltradas($projeto_id);
+        // $context['subprefeituras'] = $api->getSubPrefeituras();
+        // $context['objetivos'] = $api->getObjetivos();
+        // $context['secretarias'] = $api->getSecretarias();
+        // $context['eixos'] = $api->getEixos();
+        // $context['articulacoes'] = $api->getArticulacoes();
 
-        $context['total_metas'] = count($context['metas']);
+        // $context['total_metas'] = count($context['metas']);
 
-        $context['subprefeitura_active'] = $_GET['subprefeitura'];
-        if (!empty($_GET['subprefeitura'])) {
-            $context['filtro_so_subprefeitura'] = 'sim';
-        }
+        // $context['subprefeitura_active'] = $_GET['subprefeitura'];
+        // if (!empty($_GET['subprefeitura'])) {
+        //     $context['filtro_so_subprefeitura'] = 'sim';
+        // }
         return $context;
     }
 }
